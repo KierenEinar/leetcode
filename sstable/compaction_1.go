2d6bbe5c11e879c595252b03aeb009b679f1f362
jianghongbin@cmcm.com <jianghongbin@cmcm.com>
2022-11-23 11:35:12 +0800

    add compaction1 expand

--- sstable/compaction_1.go
+++ sstable/compaction_1.go
@@ -27,6 +27,8 @@ func (vSet *VersionSet) pickCompaction1() *compaction1 {
 	}
 
 	cLevel := vSet.current.cLevel
+	assert(cLevel < kLevelNum)
+
 	level := vSet.current.levels[cLevel]
 
 	inputs := make(tFiles, 0)
@@ -53,7 +55,7 @@ func (vSet *VersionSet) pickCompaction1() *compaction1 {
 func newCompaction1(inputs tFiles, cPtr compactPtr, levels Levels, cmp BasicComparer) *compaction1 {
 
 	c := &compaction1{
-		inputs: [2]tFiles{inputs, nil},
+		inputs: [2]tFiles{inputs},
 		levels: levels,
 		cPtr:   cPtr,
 		cmp:    cmp,
@@ -64,18 +66,23 @@ func newCompaction1(inputs tFiles, cPtr compactPtr, levels Levels, cmp BasicComp
 
 func (c *compaction1) expand() {
 
-	inputLevel := c.inputs[0]
-	compactLevel := c.inputs[1]
+	t0, t1 := c.inputs[0], c.inputs[1]
 
+	vs0, vs1 := c.levels[c.cPtr.level], c.levels[c.cPtr.level+1]
+
+	imin, imax := append(t0, t1...).getRange1(c.cmp)
 	if c.cPtr.level == 0 {
+		vs0.getOverlapped1(&t0, imin, imax, true)
 
-		inputLevel = c.levels[0].getOverlapped()
-
+		// recalculate the imin and imax
+		imin, imax = append(t0, t1...).getRange1(c.cmp)
 	}
+
+	vs1.getOverlapped1(&t1, imin, imax, false)
 
 }
 
-func (tFiles tFiles) getOverlapped1(dst tFiles, imin InternalKey, imax InternalKey, overlapped bool) {
+func (tFiles tFiles) getOverlapped1(dst *tFiles, imin InternalKey, imax InternalKey, overlapped bool) {
 
 	umin := imin.ukey()
 	umax := imax.ukey()
@@ -83,15 +90,15 @@ func (tFiles tFiles) getOverlapped1(dst tFiles, imin InternalKey, imax InternalK
 	if overlapped {
 		i := 0
 		for ; i < len(tFiles); i++ {
-			if tFiles[i].overlapped(imin, imax) {
+			if tFiles[i].overlapped1(imin, imax) {
 				tMinR := bytes.Compare(tFiles[i].iMin.ukey(), umin)
 				tMaxR := bytes.Compare(tFiles[i].iMax.ukey(), umax)
 
 				if tMinR >= 0 && tMaxR <= 0 {
-					dst = append(dst, tFiles[i])
+					*dst = append(*dst, tFiles[i])
 				} else {
 					i = 0
-					dst = dst[:0]
+					*dst = (*dst)[:0]
 					if tMinR < 0 {
 						umin = tFiles[i].iMin.ukey()
 					}
@@ -133,15 +140,27 @@ func (tFiles tFiles) getOverlapped1(dst tFiles, imin InternalKey, imax InternalK
 		}
 
 		assert(end >= begin)
-		dst = append(dst, tFiles[begin:end]...)
+		*dst = append(*dst, tFiles[begin:end]...)
 	}
 
 }
 
-func (a tFile) overlapped(imin InternalKey, imax InternalKey) bool {
-	if bytes.Compare(a.iMax.ukey(), imin.ukey()) < 0 ||
-		bytes.Compare(a.iMin.ukey(), imax.ukey()) > 0 {
+func (tFile tFile) overlapped1(imin InternalKey, imax InternalKey) bool {
+	if bytes.Compare(tFile.iMax.ukey(), imin.ukey()) < 0 ||
+		bytes.Compare(tFile.iMin.ukey(), imax.ukey()) > 0 {
 		return false
 	}
 	return true
+}
+
+func (tFiles tFiles) getRange1(cmp BasicComparer) (imin, imax InternalKey) {
+	for _, tFile := range tFiles {
+		if cmp.Compare(tFile.iMin, imin) < 0 {
+			imin = tFile.iMin
+		}
+		if cmp.Compare(tFile.iMax, imax) > 0 {
+			imax = tFile.iMax
+		}
+	}
+	return
 }
